ed67ecd8ec2560f5dc8afc76730b9fcd4613cf3a
alexcoca <alexcoca23@yahoo.co.uk>
2025-07-31 15:33:38 +0100

    Remove base path for user site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alexcoca.github.io',
-  base: '/personal-website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
